d4249aeb39f185ff159ef3b4338675498dddb16c
Dhamodharabalaji R <Dhamodharabalaji R>
2017-02-24 21:31:27 +0530

    added working version mongoose

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
-var firebase = require("firebase");
+// var firebase = require("firebase");
+var mongoose = require('mongoose');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -21,25 +22,65 @@ app.listen(app.get('port'), function() {
 
 // Initialize Firebase
 // TODO: Replace with your project's customized code snippet
- var config = {
-    apiKey: "AIzaSyBr0sEJAfB1XUtFemcFOp8WfBaXEPwV434",
-    authDomain: "simpleonenode.firebaseapp.com",
-    databaseURL: "https://simpleonenode.firebaseio.com",
-    storageBucket: "simpleonenode.appspot.com",
-    messagingSenderId: "889210340292"
-  };
-firebase.initializeApp(config);
+//  var config = {
+//     apiKey: "AIzaSyBr0sEJAfB1XUtFemcFOp8WfBaXEPwV434",
+//     authDomain: "simpleonenode.firebaseapp.com",
+//     databaseURL: "https://simpleonenode.firebaseio.com",
+//     storageBucket: "simpleonenode.appspot.com",
+//     messagingSenderId: "889210340292"
+//   };
+// firebase.initializeApp(config);
 
-
-var FirebaseClient = require('firebase-client');
-var firebaseCli = new FirebaseClient({
-  url: "https://simpleonenode.firebaseio.com/",
-  auth: "AIzaSyBr0sEJAfB1XUtFemcFOp8WfBaXEPwV434"
+// var database = firebase.database();
+ mongoose.connect('mongodb://dhamofficial:1qaz2wsx@ds058369.mlab.com:58369/simpleonenode');
+//  mongoose.connect('mongodb://localhost/myappdatabase');
+ var Schema = mongoose.Schema;
+ var userSchema = new Schema({
+  name: String,
+  username: { type: String, required: true, unique: false },
+  password: { type: String, required: true },
+  admin: Boolean,
+  location: String,
+  meta: {
+    age: Number,
+    website: String
+  },
+  created_at: Date,
+  updated_at: Date
 });
 
-firebaseCli
-.push('example', { value: true });
-console.log('set');
+// the schema is useless so far
+// we need to create a model using it
+var User = mongoose.model('User', userSchema);
 
-var v=firebaseCli.get('example');
-console.log('get=',v);
+// make this available to our users in our Node applications
+module.exports = User;
+
+
+var chris = new User({
+  name: 'Chris',
+  username: 'sevilayha',
+  password: 'password' 
+});
+
+// call the custom method. this will just add -dude to his name
+// user will now be Chris-dude
+// chris.dudify(function(err, name) {
+//   if (err) throw err;
+
+//   console.log('Your new name is ' + name);
+// });
+
+// call the built-in save method to save to the database
+chris.save(function(err) {
+  if (err) throw err;
+
+  console.log('User saved successfully!');
+});
+
+User.find({}, function(err, users) {
+  if (err) throw err;
+
+  // object of all the users
+  console.log(users);
+});
