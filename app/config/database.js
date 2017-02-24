d1a53bddae6464940bbc545d6e72c9efe892164e
Dhamodharabalaji R <Dhamodharabalaji R>
2017-02-24 22:57:28 +0530

    adding todo program

---
+++ app/config/database.js
@@ -0,0 +1,9 @@
+// module.exports = {
+//     remoteUrl : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu',
+//     localUrl: 'mongodb://localhost/meanstacktutorials'
+// };
+
+module.exports = {
+    remoteUrl : 'mongodb://dhamofficial:1qaz2wsx@ds058369.mlab.com:58369/simpleonenode',
+    localUrl: 'mongodb://localhost/meanstacktutorials'
+};
