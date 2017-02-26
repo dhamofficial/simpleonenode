31b5f548a7afcb9093c0146e6b1249f4c2677e2f
Dhamodharabalaji R <Dhamodharabalaji R>
2017-02-26 16:03:35 +0530

    concurrent execution added

--- public/js/services.js
+++ public/js/services.js
@@ -1,18 +1,19 @@
 angular
     .module('inspinia')
 	.factory('Todos', ['$http',function($http) {
+		var prefix='http://localhost:8080';
 		return {
 			get : function() {
-				return $http.get('/api/todos');
+				return $http.get(prefix+'/api/todos');
 			},
 			create : function(todoData) {
-				return $http.post('/api/todos', todoData);
+				return $http.post(prefix+'/api/todos', todoData);
 			},
 			delete : function(id) {
-				return $http.delete('/api/todos/' + id);
+				return $http.delete(prefix+'/api/todos/' + id);
 			},
 			update : function(id,data) {
-				return $http.put('/api/todos/' + id,data);
+				return $http.put(prefix+'/api/todos/' + id,data);
 			}
 		}
 	}]);
